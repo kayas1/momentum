4051466d01d4dbf8976b6e00fc558b7f4ce184bc
kayas1 <wogur1833@naver.com>
2023-08-14 19:53:44 +0900

    cleaning

--- js/background.js
+++ js/background.js
@@ -1,10 +1,10 @@
 const images=[
     "Loading6.png",
-"Loading7.png",
-"Loading8.png",
-"Loading9.png",
-"Loading10.png",
-"Loading11.png",
+    "Loading7.png",
+    "Loading8.png",
+    "Loading9.png",
+    "Loading10.png",
+    "Loading11.png",
     "Loading12.png",
 ]
 const chosenImage=images[Math.floor(Math.random()*images.length)];
