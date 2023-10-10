69ce88367302b80d7d22917e6ce41c462002b419
kayas1 <wogur1833@naver.com>
2023-10-10 20:46:27 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,6 +3,7 @@ const API_KEY="dedbd307791715f689f4cffce5984131";
 function onGeoOK(position){
     const lat=position.coords.latitude;
     const lon=position.coords.longitude;
+    console.log(lat +" "+lon);
     const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     fetch(url).then(response => response.json()).then(data=>{
         const weather=document.querySelector("#weather span:first-child");
